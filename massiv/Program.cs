5191be446b2811c56d990ab8adf60ae952d8bbaa
alex <you@example.com>
2021-10-30 16:52:14 +0300

    added ege

---
+++ massiv/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] CreateArray(int n)
+{
+       int[] a=new int[n];
+       Random random=new Random();
+       for(int i=0;i<a.Length;i++)  a[i]=random.Next(0,101);
+       return a;
+}
+
+void PrintArray(int[] a)
+{
+    for(int i=0;i<a.Length;i++) Console.Write(a[i]+" ");
+}
+
+int[] arr=CreateArray(2);
+PrintArray(arr);
+
+int p=1;
+for(int i=0;i<arr.Length;i++)
+if (arr[i]%2==0 && arr[i]%10!=0){
+    p=p*arr[i];
+}
+Console.WriteLine();
+Console.WriteLine(p);
